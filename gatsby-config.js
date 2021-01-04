fecd5e3eee02a0034807bb91efa33e83a897f090
maknetaRo <makneta@gmail.com>
2021-01-04 13:47:17 +0100

    changed path in config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: `/portfolio`,
+  pathPrefix: `/`,
   siteMetadata: {
     title: `Magda Rosłaniec Portfolio`,
     description: `My developer's portfolio, main projects and articles`,
