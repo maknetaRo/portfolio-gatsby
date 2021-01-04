1adf32486cfea3246e8994805cecadcaec4fde4a
maknetaRo <makneta@gmail.com>
2021-01-04 23:51:10 +0100

    started aboutSection component

--- src/components/aboutSection/aboutSection.js
+++ src/components/aboutSection/aboutSection.js
@@ -1,35 +1,64 @@
 import React from 'react'
+import { data } from '../../constants/links'
 import styles from './aboutSection.module.css'
+import Img from 'gatsby-image'
+import { graphql, useStaticQuery } from "gatsby"
+
 
 const AboutSection = () => {
+
+const data = useStaticQuery(graphql`
+fragment squareImage on File {
+  childImageSharp {
+    fluid(maxWidth: 200, maxHeight: 200) {
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+
+query {
+  image1: file(relativePath: { eq: "assets/lake_krzywe.jpeg" }) {
+    ...squareImage
+  }
+
+  image2: file(relativePath: { eq: "assets/spring-lake.jpg" }) {
+    ...squareImage
+  }
+
+  image3: file(relativePath: { eq: "assets/zalew-trees.jpg" }) {
+    ...squareImage
+  }
+}
+`);
+  
+  const { image1, image2, image3 } = data
     return (
-        <article className={styles.aboutArticle} class="card">
+      <section>
+        <article className={styles.card}>
         
-        <p className={styles.intro} className={styles.description}>
+        <p className={styles.intro}>
           My name's Magdalena Rosłaniec but I like my internet nickname: makneta.
           I'm a teacher, mother and self-taught developer who wants to change
           her career into programming.
         </p>
-        <div class="about-picture">
-          <img
-            src="./img/spring-lake.jpg"
+        <div className={styles.aboutPicture}>
+          <Img image={data.image1}
             alt="spring lake"
-            class="about-img"
+            className={styles.aboutImg}
           />
         </div>
-        <p class="description">
+        <p className={styles.description}>
           I live in a medium size town in the north-east Poland. I love my small
           fatherland, it's a peaceful place, full of lakes and forests, where I
           enjoy running, nordic walking and riding a bike in my free time.
         </p>
-        <div class="about-picture">
-          <img
-            src="./img/zalew-trees.jpg"
+        <div className={styles.aboutPicture}>
+        <Img image={data.image2}
             alt="spring lake"
-            class="about-img"
+            className={styles.aboutImg}
           />
         </div>
-        <p class="description">
+        <p className={styles.description}>
           I started my adventure with programming in September 2017 and since
           then I've been slowly but steadily learning Python and Django. I've
           learnt also a bit of SQL, JavaScript and C++ but Python is deeply in
@@ -37,26 +66,26 @@ const AboutSection = () => {
           pleasure.
         </p>
         
-        <p class="description">
-          I'm taking part in a #100DaysOfCode movement and everyday teach myself
+        <p className={styles.description}>
+          I take part in a #100DaysOfCode movement and everyday teach myself
           something new. I'm a driven by these words:
          
-          <blockquote>
+          <blockquote className={styles.blockquote}>
             “Don't wish it was easier
             wish you were better. Don't wish for less problems wish for more
-            skills. Don't wish for less challenge wish for more wisdom”
+            skills. Don't wish for less challenge wish for more wisdom” 
             <em>Jin Rohn</em> 
         </blockquote> 
           
         </p>
-        <div class="about-picture">
-            <img
-              src="./img/lake_krzywe.jpeg"
+        <div className={styles.aboutPicture}>
+        <Img image={data.image3}
               alt="spring lake"
-              class="about-img"
+              className={styles.aboutImg}
             />
         </div>
         </article>
+      </section>
     )
 }
 
